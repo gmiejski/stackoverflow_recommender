b400d6ac0b75601b6d6b0f3e85be6340afa1bbec
Grzegorz Miejski <grzegorz.miejski@allegrogroup.com>
2015-10-18 14:04:23 +0200

    starting project

---
+++ src/main/java/miejski/org/stackoverflow/StackoverflowRecommenderApplication.java
@@ -0,0 +1,12 @@
+package miejski.org.stackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowRecommenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackoverflowRecommenderApplication.class, args);
+    }
+}
---
+++ src/test/java/miejski/org/stackoverflow/StackoverflowRecommenderApplicationTests.java
@@ -0,0 +1,16 @@
+package miejski.org.stackoverflow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = StackoverflowRecommenderApplication.class)
+public class StackoverflowRecommenderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
